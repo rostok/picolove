75ddaae369707037ed3307d3cac67098c804b64f
rostok <rostok@3e.pl>
2023-03-17 01:57:32 +0100

    custom error handler with printing line at traceback F5 restarts love (may be broken)

--- main.lua
+++ main.lua
@@ -680,7 +680,7 @@ function flip_screen()
 end
 
 function love.focus(f)
-	focus = f
+	focus = f or __pico_nofocus_update
 end
 
 local function lowpass(y0, y1, cutoff) -- luacheck: no unused
@@ -882,6 +882,10 @@ function love.keypressed(key)
 	if key == "r" and isCtrlOrGuiDown() and not isAltDown() then
 		api.reload_cart()
 		api.run()
+	elseif key == "f5" then
+		package.loaded["conf"] = nil
+		love.event.quit('restart')
+		love.load({__pico_cart})
 	elseif
 		key == "escape"
 		and cartname ~= nil
@@ -1075,3 +1079,165 @@ function love.run()
 	end
 end
 
+local utf8 = require("utf8")
+
+function add_code_to_traceback(trace,extra)
+    extra = extra or ""
+	local lines = {}
+	for line in love.filesystem.lines(__pico_cart) do table.insert(lines, line) end
+
+    local err = {}
+	for l in trace:gmatch("(.-)\n") do
+			table.insert(err, l)
+	end
+
+    for i, errMsg in ipairs(err) do
+      local l = errMsg:gsub("\t", ""):gsub("%[string \"(.-)\"%]", "%1") 
+      if l:sub(1,__pico_cart:len())==__pico_cart then
+        local lineNumber = tonumber(string.match(errMsg, ":%d+:"):sub(2, -2))
+        local line = lineNumber..":"..lines[lineNumber]
+        table.insert(err, i+1, extra)
+        table.insert(err, i+1, extra.."  >>  "..line)
+      end
+    end
+	return table.concat(err, "\n")
+end
+
+local function error_printer(msg, layer)
+    local trace = debug.traceback("Error: " .. tostring(msg), 1+(layer or 1))
+    trace = add_code_to_traceback(trace,"\n\t")
+	print((trace:gsub("\n[^\n]+$", "")))
+end
+
+function love.errorhandler(msg)
+	msg = tostring(msg)
+
+	error_printer(msg, 2)
+
+	if not love.window or not love.graphics or not love.event then
+		return
+	end
+
+	if not love.graphics.isCreated() or not love.window.isOpen() then
+		local success, status = pcall(love.window.setMode, 800, 600)
+		if not success or not status then
+			return
+		end
+	end
+
+	-- Reset state.
+	if love.mouse then
+		love.mouse.setVisible(true)
+		love.mouse.setGrabbed(false)
+		love.mouse.setRelativeMode(false)
+		if love.mouse.isCursorSupported() then
+			love.mouse.setCursor()
+		end
+	end
+	if love.joystick then
+		-- Stop all joystick vibrations.
+		for i,v in ipairs(love.joystick.getJoysticks()) do
+			v:setVibration()
+		end
+	end
+	if love.audio then love.audio.stop() end
+
+	love.graphics.reset()
+	local font = love.graphics.setNewFont(14)
+
+	love.graphics.setColor(1, 1, 1)
+
+	local trace = debug.traceback()
+    trace = add_code_to_traceback(trace,"\t")
+
+	love.graphics.origin()
+
+	local sanitizedmsg = {}
+	for char in msg:gmatch(utf8.charpattern) do
+		table.insert(sanitizedmsg, char)
+	end
+	sanitizedmsg = table.concat(sanitizedmsg)
+
+	local err = {}
+
+	table.insert(err, "Error\n")
+	table.insert(err, sanitizedmsg)
+
+	if #sanitizedmsg ~= #msg then
+		table.insert(err, "Invalid UTF-8 string in error message.")
+	end
+
+	table.insert(err, "\n")
+
+	for l in trace:gmatch("(.-)\n") do
+		if not l:match("boot.lua") then
+			l = l:gsub("stack traceback:", "Traceback\n")
+			table.insert(err, l)
+		end
+	end
+
+	local p = table.concat(err, "\n")
+	p = p:gsub("\t", "")
+	p = p:gsub("%[string \"(.-)\"%]", "%1")
+
+	local function draw()
+		if not love.graphics.isActive() then return end
+		local pos = 32
+		love.graphics.clear(89/255, 157/255, 220/255)
+		love.graphics.printf(p, pos, pos, love.graphics.getWidth() - pos)
+		love.graphics.present()
+	end
+
+	local fullErrorText = p
+	local function copyToClipboard()
+		if not love.system then return end
+		love.system.setClipboardText(fullErrorText)
+		p = p .. "\nCopied to clipboard!"
+	end
+
+	if love.system then
+		p = p .. "\n\nPress Ctrl+C or tap to copy this error" .. "\n\nPress Ctrl+R to restart"
+	end
+
+	return function()
+		love.event.pump()
+
+		for e, a, b, c in love.event.poll() do
+			if e == "quit" then
+				return 1
+			elseif e == "keypressed" and a == "escape" then
+				return 1
+			elseif e == "keypressed" and a == "c" and love.keyboard.isDown("lctrl", "rctrl") then
+				copyToClipboard()
+			elseif e == "keypressed" and a == "r" and love.keyboard.isDown("lctrl", "rctrl") then
+                package.loaded["conf"] = nil
+                --love.event.quit('restart')
+                --love.load({__pico_cart})
+                --love.graphics.setCanvas()
+                api.reload_cart()
+                api.run()
+                return
+			elseif e == "touchpressed" then
+				local name = love.window.getTitle()
+				if #name == 0 or name == "Untitled" then name = "Game" end
+				local buttons = {"OK", "Cancel"}
+				if love.system then
+					buttons[3] = "Copy to clipboard"
+				end
+				local pressed = love.window.showMessageBox("Quit "..name.."?", "", buttons)
+				if pressed == 1 then
+					return 1
+				elseif pressed == 3 then
+					copyToClipboard()
+				end
+			end
+		end
+
+		draw()
+
+		if love.timer then
+			love.timer.sleep(0.1)
+		end
+	end
+
+end
